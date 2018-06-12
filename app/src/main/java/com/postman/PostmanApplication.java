d5f63446fc83953cb1c04af4f5bc002de0caf014
chengjunfeng <sjun945@outlook.com>
2018-06-12 09:05:37 +0800

    应用升级：接入Bugly应用升级与日志管理

--- app/src/main/java/com/postman/PostmanApplication.java
+++ app/src/main/java/com/postman/PostmanApplication.java
@@ -2,11 +2,19 @@ package com.postman;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
 import android.support.multidex.MultiDexApplication;
 
+import com.base.config.GlobalConfig;
 import com.blankj.utilcode.util.Utils;
 import com.base.utils.LogUtil;
 import com.postman.db.control.AppDbManager;
+import com.postman.ui.module.other.upgrade.UpgradeActvity;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.UpgradeInfo;
+import com.tencent.bugly.beta.upgrade.UpgradeListener;
 import com.yanzhenjie.nohttp.NoHttp;
 
 import java.util.List;
@@ -44,6 +52,34 @@ public class PostmanApplication extends MultiDexApplication {
         AppDbManager.init(mContext);
         //nohttp
         NoHttp.initialize(this);
+        //upgrade
+        initUpgrade();
+    }
+
+    private void initUpgrade(){
+        Beta.autoInit = true;
+        Beta.autoCheckUpgrade = true;
+        Beta.initDelay = 1 * 1000;
+        Beta.largeIconId = R.mipmap.ic_launcher;
+        Beta.smallIconId = R.mipmap.ic_launcher;
+        Beta.defaultBannerId = R.mipmap.ic_launcher;
+        Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        Beta.showInterruptedStrategy = false;
+        Beta.upgradeListener = new UpgradeListener() {
+            @Override
+            public void onUpgrade(int ret, UpgradeInfo strategy, boolean isManual, boolean isSilence) {
+                LogUtil.d(TAG, "onUpgrade:"+isManual);
+                if (strategy != null) {
+                    Intent i = new Intent();
+                    i.setClass(getApplicationContext(), UpgradeActvity.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                } else {
+                    //Toast.makeText(getApplicationContext(), "没有更新", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        Bugly.init(getApplicationContext(), "24679767ee", GlobalConfig.IS_DEBUG);
     }
 
     private boolean shouldInit() {
